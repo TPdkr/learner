efc376f39240671e72cefa749fa6783b77a4f374
tpdkrt <tpodko@gmail.com>
2025-01-01 11:28:31 +0500

    simple lesson activity prototype is working

--- app/src/main/java/com/example/learner/ui/LessonScreen.kt
+++ app/src/main/java/com/example/learner/ui/LessonScreen.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -17,6 +19,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,6 +27,9 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -33,13 +39,16 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.learner.R
 import com.example.learner.ui.viewModels.LessonViewModel
 
 @Preview
 @Composable
-fun LessonScreen(lessonViewModel: LessonViewModel = LessonViewModel()) {
+fun LessonScreen(lessonViewModel: LessonViewModel = viewModel()) {
     val lessonUiState by lessonViewModel.uiState.collectAsState()
+    var isSubmitted by remember { mutableStateOf(false) }
     Surface(modifier = Modifier.fillMaxSize()) {
 
     }
@@ -58,7 +67,7 @@ fun LessonScreen(lessonViewModel: LessonViewModel = LessonViewModel()) {
         Text(
             text = stringResource(
                 R.string.task_number,
-                lessonUiState.taskNumber,
+                lessonUiState.taskNumber+1,
                 lessonUiState.taskCount
             ), style = typography.bodyLarge, modifier = Modifier.padding(20.dp)
         )
@@ -97,12 +106,12 @@ fun LessonScreen(lessonViewModel: LessonViewModel = LessonViewModel()) {
                         disabledContainerColor = colorScheme.surface,
                     ),
                     label = { Text(text = "enter translation") },
-                    isError = false,
+                    isError = lessonUiState.isWrong,
                     keyboardOptions = KeyboardOptions.Default.copy(
                         imeAction = ImeAction.Done
                     ),
                     keyboardActions = KeyboardActions(
-                        onDone = { }//TODO!!!!!
+                        onDone = { }//TODO!!!!!Sch
                     )
                 )
             }
@@ -113,14 +122,73 @@ fun LessonScreen(lessonViewModel: LessonViewModel = LessonViewModel()) {
                 .fillMaxSize()
                 .padding(40.dp)
         ) {
-            Button(onClick = {}, modifier = Modifier.width(300.dp)) {//TODO!!!!
-                Text(text = "Check")
+            if (!lessonUiState.isChecked) {
+                Button(
+                    onClick = { lessonViewModel.checkAnswer() },
+                    modifier = Modifier.width(300.dp)
+                ) {
+                    Text(text = "Check")
+                }
+            } else if (lessonUiState.taskNumber+1==lessonUiState.taskCount){
+                OutlinedButton(onClick = { isSubmitted=true}, modifier = Modifier.width(300.dp)) {
+                    Text(text = "Submit")
+                }
+            }else {
+                OutlinedButton(onClick = {lessonViewModel.nextTask()}, modifier = Modifier.width(300.dp)) {
+                    Text(text = "Next")
+                }
             }
         }
+    }
+    if (isSubmitted){
+        FinalDialog(lessonUiState.score)
     }
 }
 
 @Composable
 fun TaskCard() {
 
+}
+
+@Composable
+fun FinalDialog(score: Int){
+    Dialog(onDismissRequest = {}) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .padding(5.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = "You finished a lesson!",
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentSize(Alignment.Center)
+                )
+                Text(
+                    text = "Good boy ;)",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentSize(Alignment.Center),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = stringResource(R.string.you_earned_xp, score),
+                    textAlign = TextAlign.Center,
+                    style = typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentSize(Alignment.Center)
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/learner/ui/viewModels/LessonViewModel.kt
+++ app/src/main/java/com/example/learner/ui/viewModels/LessonViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.learner.ui.viewModels
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -16,36 +17,75 @@ class LessonViewModel : ViewModel() {
     //Game ui state
     private val _uiState = MutableStateFlow(LessonUiState())
     val uiState: StateFlow<LessonUiState> = _uiState.asStateFlow()
+
     //this is the lesson data
     private val currentLesson: Lesson = testLesson
+
     //this is the current word
     private lateinit var currentWord: Word
+
     //we start the lesson
-    init{
+    init {
         startLesson()
     }
+
     var userGuess by mutableStateOf("")
         private set
-    fun startLesson(){
-        currentWord=currentLesson.words.first()
-        _uiState.value=LessonUiState(currentTrans = currentWord.translation)
+
+    //Start the lesson and set the default values
+    private fun startLesson() {
+        Log.d("Lesson error", "Start lesson is called")
+        if (currentLesson.words.isNotEmpty()) {
+            currentWord = currentLesson.words.first()
+            _uiState.value = LessonUiState(
+                currentTrans = currentWord.translation,
+                taskCount = currentLesson.words.size
+            )
+        }
     }
-    fun updateUserGuess(newGuess: String){
-        userGuess=newGuess
+
+    //update user guess after change
+    fun updateUserGuess(newGuess: String) {
+        userGuess = newGuess
     }
-    fun nextTask(){
-        TODO()
+
+    //move to next task if possible
+    fun nextTask() {
+        if (_uiState.value.taskNumber<_uiState.value.taskCount-1) {
+            val nextTaskNumber=_uiState.value.taskNumber+1
+            currentWord = currentLesson.words[nextTaskNumber]
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isChecked = false,
+                    isWrong = false,
+                    taskNumber = nextTaskNumber,
+                    currentTrans = currentWord.translation
+                )
+            }
+            updateUserGuess("")
+        } else {
+            //need to handle the end of the lesson
+        }
+        Log.d("Lesson error",currentWord.german)
+
     }
-    fun checkAnswer(){
-        TODO()
+
+    //check the input
+    fun checkAnswer() {
+        val isCorrect = userGuess.equals(currentWord.german, ignoreCase = true)
+        _uiState.update { currentState ->
+            val newScore = if (isCorrect) currentState.score.plus(20) else currentState.score
+            currentState.copy(score = newScore, isChecked = true, isWrong = !isCorrect)
+        }
     }
 }
 
 data class LessonUiState(
     val isChecked: Boolean = false,
+    val isWrong: Boolean = false,
     val taskNumber: Int = 0,
-    val taskCount: Int = 15,
+    val taskCount: Int = 0,
     val score: Int = 0,
     val wordCount: Int = 0,
-    val currentTrans: String=""
+    val currentTrans: String = ""
 )
