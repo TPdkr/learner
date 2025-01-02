a8c8332c1633d2098f658f9281be7c70bf2033ef
tpdkrt <tpodko@gmail.com>
2025-01-02 14:26:51 +0500

    added unit card with detailed description

--- app/src/main/java/com/example/learner/ui/UnitScreen.kt
+++ app/src/main/java/com/example/learner/ui/UnitScreen.kt
@@ -12,11 +12,16 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -24,13 +29,18 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.example.learner.R
 import com.example.learner.classes.Course
 import com.example.learner.classes.CourseUnit
+import com.example.learner.classes.Gender
 import com.example.learner.data.testCourse
+import com.example.learner.data.testUnit
 
 @Composable
 fun UnitScreen(course: Course) {
+    var showUnit = remember { mutableStateOf(false) }
+    var currentUni = remember { mutableStateOf(testUnit) }
     val units = course.units.chunked(2)
     Surface(
         modifier = Modifier
@@ -50,6 +60,10 @@ fun UnitScreen(course: Course) {
                     Row {
                         UnitCard(
                             pair[0],
+                            {
+                                showUnit.value = true
+                                currentUni.value = pair[0]
+                            },
                             modifier = Modifier
                                 .weight(1f)
                                 .padding(top = 8.dp, end = 8.dp, start = 8.dp)
@@ -57,6 +71,10 @@ fun UnitScreen(course: Course) {
                         if (pair.size == 2) {
                             UnitCard(
                                 pair[1],
+                                {
+                                    showUnit.value = true
+                                    currentUni.value = pair[1]
+                                },
                                 modifier = Modifier
                                     .weight(1f)
                                     .padding(top = 8.dp, end = 8.dp)
@@ -73,14 +91,23 @@ fun UnitScreen(course: Course) {
             }
         }
     }
+    if (showUnit.value) {
+        UnitDetailedCard(currentUni.value, { showUnit.value = false })
+    }
 }
 
 @Composable
-fun UnitCard(unit: CourseUnit, modifier: Modifier = Modifier) {
-    Card(modifier.height(70.dp)) {
-        Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(5.dp)) {
+fun UnitCard(unit: CourseUnit, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.height(70.dp), onClick = onClick) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(5.dp)
+        ) {
             Column(
-                modifier = Modifier.fillMaxSize().weight(4f),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(4f),
             ) {
                 Text(
                     text = stringResource(R.string.unit_number, unit.number),
@@ -89,10 +116,81 @@ fun UnitCard(unit: CourseUnit, modifier: Modifier = Modifier) {
                 )
                 Text(text = unit.name)
             }
-            CircularProgressIndicator(progress = {0.8F}, modifier = Modifier.fillMaxSize().weight(1f))
+            CircularProgressIndicator(
+                progress = { 0.8F },
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            )
         }
     }
 
+}
+
+@Composable
+fun UnitDetailedCard(unit: CourseUnit, onDismissRequest: () -> Unit) {
+    Dialog(onDismissRequest = onDismissRequest) {
+        Card(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                ) {
+                    Text(
+                        text = stringResource(R.string.unit_number, unit.number),
+                        style = typography.titleLarge,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                    )
+                    Text(text = unit.name, style = typography.titleMedium)
+                    Text(text = unit.desc)
+                }
+                Text(text = "Words in this unit:", fontWeight = FontWeight.Bold)
+                Card(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(8f),
+                    colors = CardColors(containerColor = MaterialTheme.colorScheme.background,
+                        contentColor = MaterialTheme.colorScheme.onBackground,
+                        disabledContentColor = MaterialTheme.colorScheme.onBackground,
+                        disabledContainerColor = MaterialTheme.colorScheme.background)
+                ) {
+                    LazyColumn(modifier = Modifier.fillMaxSize().padding(10.dp)) {
+                        items(unit.word) { word ->
+                            Row(modifier = Modifier.height(25.dp), verticalAlignment = Alignment.CenterVertically) {
+                                when(word.gender){
+                                    Gender.DER-> Text("Der ")
+                                    Gender.DAS->Text("Das ")
+                                    Gender.DIE->Text("Die ")
+                                    else->Text("")
+                                }
+                                Text(word.german)
+                                Text(" = ")
+                                Text(word.translation)
+                            }
+                            HorizontalDivider(thickness = 2.dp)
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CardPreview() {
+    Surface {
+        UnitDetailedCard(testUnit) { }
+    }
 }
 
 @Preview
