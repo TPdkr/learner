7252b93b49ea390656c3f2de9efdc93f946e783f
tpdkrt <tpodko@gmail.com>
2025-01-02 12:29:20 +0500

    optimized imports

--- app/src/main/java/com/example/learner/UnitActivity.kt
+++ app/src/main/java/com/example/learner/UnitActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.learner.data.testLesson
-import com.example.learner.ui.LessonScreen
 import com.example.learner.ui.theme.LearnerTheme
-import com.example.learner.ui.viewModels.LessonViewModel
 
 class UnitActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
