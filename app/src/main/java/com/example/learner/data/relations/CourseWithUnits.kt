d6f99c73df26adb79b6ee92685a5f9af1a1a231a
tpdkrt <tpodko@gmail.com>
2025-01-15 12:32:51 +0500

    commented out unused class

--- app/src/main/java/com/example/learner/data/relations/CourseWithUnits.kt
+++ app/src/main/java/com/example/learner/data/relations/CourseWithUnits.kt
@@ -1,13 +1,8 @@
 package com.example.learner.data.relations
 
-import androidx.room.Embedded
-import androidx.room.Relation
-import com.example.learner.data.course.CourseEntity
-import com.example.learner.data.unit.UnitEntity
-
 /**this data class stores the info about the relation between units and courses. It is 1:n relation
  * as one course can relate to 0 or many units*/
-data class CourseWithUnits (
+/*data class CourseWithUnits (
     @Embedded val courseEntity: CourseEntity,
     @Relation(
         parentColumn = "cid",//in courses table
@@ -16,4 +11,4 @@ data class CourseWithUnits (
     )
     val units: List<CourseEntity>
 )
-//methods to fetch this are in the course package in ..
+//methods to fetch this are in the course package in ..*/
